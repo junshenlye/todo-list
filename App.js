0e49fbcd61801bd463ce7be2c3008981ad6a79d0
junshenlye <junshenlye@gmail.com>
2024-04-19 00:46:41 +0800

    tried reusable components

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {TaskCard} from './components/task'
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
         <Text style={styles.title}>Today's Tasks</Text> 
         <View style={styles.items}>
-
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
         </View>
       </View>
     </View>
---
+++ components/task.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function TaskCard() {
+    return(
+        <View>
+            <Text>Something</Text>
+        </View>
+    );
+};
+
