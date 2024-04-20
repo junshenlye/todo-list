aaefcf78284f918550d13508f1779496d45cf9e2
junshenlye <junshenlye@gmail.com>
2024-04-20 23:07:28 +0800

    addition

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
             <TaskCard></TaskCard>
             <TaskCard></TaskCard>
             <TaskCard></TaskCard>
+            <TaskCard></TaskCard>
         </View>
       </View>
     </View>
