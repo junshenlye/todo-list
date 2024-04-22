52093b3e9314e965068912c029f9e6d5be4a9779
junshenlye <junshenlye@gmail.com>
2024-04-22 22:22:46 +0800

    adding additional to task card

--- components/task.js
+++ components/task.js
@@ -5,6 +5,7 @@ export function TaskCard() {
         <View>
             <Text>Something</Text>
             <Text>I slacked today, i do this tmr </Text>
+            <Text>Let the tech Debt stacks X 2</Text>
         </View>
     );
 };
